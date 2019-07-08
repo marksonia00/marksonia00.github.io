2c58101726100897c4909d2a961296817571e903
marksonia00 <marksonia00@gmail.com>
2019-07-08 11:19:19 +0800

    jquery

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+$("#cb1").mouseover(function(){$("body").css("background-color","red");})
+$("#cb2").mouseover(function(){$("body").css("background-color","yellow");})
+$("#cb3").mouseover(function(){$("body").css("background-color","green");})
+$(".container_block").mouseleave(function(){$("body").css("background-color","black");})
+
 const app = new Vue({
     el:"app",
     data:{
